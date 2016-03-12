d4cb514fce4e55713cb60f78f47b92c9aa1ec784
Lars Otten <mail@lotten.net>
2016-03-11 23:01:09 -0800

    First working, barebones version.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # gridGen
-Randomly generate grid-shaped graphical models in UAI file format
+Randomly generates grid-shaped graphical models in UAI file format [1].
+
+Copyright (c) 2016, Lars Otten (<lotten@uci.edu>), licensed under MIT license.
+
+## Usage
+Call with `gridGen.py N [det]`, where *N* is the size of the grid (N x N) and *det* (optional) is the level of determinism, i.e. the probability that any given cost function value is 0.
+
+### Examples
+- `gridGen.py 10 .1` for a 10x10 grid with 10% determinism.
+- `gridGen.py 15 0` for a 15x15 grid with no determinism.
+
+## Requirements
+Python 2 or 3.
+
+## Links
+[1] <http://graphmod.ics.uci.edu/uai08/FileFormat>
---
+++ gridGen.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+
+import sys
+import os
+import random
+
+DOMAIN_SIZE = 2
+NETWORK_TYPE = 'MARKOV'
+FLOAT_FORMAT = '%.3f'
+
+def main(size, deteterminism):
+  print NETWORK_TYPE
+  # Domain info
+  no_var = size ** 2
+  print no_var
+  for i in range(no_var):
+    print DOMAIN_SIZE,
+  print
+
+  # Number of functions
+  no_fun = 2 * size * (size-1)
+  print no_fun
+
+  # Function scope info, horizontal edges first
+  for row in range(size):
+    for col in range(size-1):
+      print '2', col + row * size, col + 1 + row * size
+  for col in range(size):
+    for row in range(size-1):
+      print '2', col + row * size, col + size + row * size  
+
+  # Randomly generated function tables
+  table_size = DOMAIN_SIZE ** 2
+  for i in range(no_fun):
+    print
+    print table_size
+    for j in range(DOMAIN_SIZE):
+      for k in range(DOMAIN_SIZE):
+        if random.random() < deteterminism:
+          print FLOAT_FORMAT % 0,
+        else:
+          print FLOAT_FORMAT % (1-random.random()),
+      print
+
+if __name__ == '__main__':
+  if len(sys.argv) < 2:
+    print 'Need to specify grid size and (optionally) level of determinism.'
+  else:
+    size = int(sys.argv[1])
+    determinism = float(sys.argv[2]) if len(sys.argv) > 2 else .5
+    if determinism < 0 or determinism > 1:
+      print 'Invalid determinism value %f, required within [0,1].' % determinism
+    else:
+      random.seed()
+      main(size, determinism)
